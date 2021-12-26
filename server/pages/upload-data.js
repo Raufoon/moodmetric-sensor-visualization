8ba53e0a9fe0edaa70800a5632dbf7ca147955c1
Minhaz Raufoon <minhaz.raufoon@s2019.tu-chemnitz.de>
2021-12-26 21:00:36 +0100

    Complete the csv upload form

--- server/pages/upload-data.js
+++ server/pages/upload-data.js
@@ -1,22 +1,35 @@
 import { useState } from "react"
+import axios from "axios"
 import PageContainer from "../components/PageContainer"
 import styles from "../styles/DataUpload.module.css"
 
-async function onSubmitUserData(values) {
-
-}
-
 export default function DataUpload() {
-  const [userId, setUserId] = useState(null);
+  const [userId, setUserId] = useState("");
   const [userDataFile, setUserDataFile] = useState(null);
+
+  async function onSubmitUserData(event) {
+    event.preventDefault()
+
+    const data = new FormData()
+    data.append("user-id", userId)
+    data.append("user-data-csv-file", userDataFile)
+
+    try {
+      const response = await axios.post("api/upload-csv", data);
+      console.log(response)
+      alert("Data uploaded successfully")
+    }
+    catch (err) {
+      alert("Error:", err.message)
+    }
+  }
 
   function onChange(event) {
     switch (event.target.name) {
       case "user-id":
         {
           const value = event.target.value
-          if (value && value.length > 4)
-            setUserId(value)
+          setUserId(value)
           break
         };
 
@@ -32,7 +45,11 @@ export default function DataUpload() {
   return (
     <PageContainer>
       <h1>Upload user data</h1>
-      <form className={styles.formContainer} onSubmit={onSubmitUserData}>
+
+      <form
+        className={styles.formContainer}
+        onSubmit={onSubmitUserData}
+      >
         <label htmlFor="user-id">User ID</label>
         <input name="user-id" onChange={onChange} placeholder="at least 5 characters" value={userId} />
 
@@ -41,7 +58,7 @@ export default function DataUpload() {
         <input type="file" onChange={onChange} name="user-data-csv-file" accept=".csv" />
 
         {
-          !!userId && !!userDataFile && <>
+          userId?.length > 4 && !!userDataFile && <>
             <br />
             <input type="submit" value="Submit" />
           </>
