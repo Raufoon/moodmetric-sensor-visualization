00ce53d413e64d89d74b5068389a4b3fbcb29065
Minhaz Raufoon <minhaz.raufoon@s2019.tu-chemnitz.de>
2021-12-26 20:28:48 +0100

    Create form for csv upload

--- server/pages/upload-data.js
+++ server/pages/upload-data.js
@@ -1,5 +1,26 @@
-import PageContainer from "../components/PageContainer";
+import PageContainer from "../components/PageContainer"
+import styles from "../styles/DataUpload.module.css"
+
+async function onSubmitUserData(values) {
+
+}
 
 export default function DataUpload() {
-  return <PageContainer>Upload data</PageContainer>
+  return (
+    <PageContainer>
+      <form className={styles.formContainer} onSubmit={onSubmitUserData}>
+        <label htmlFor="user-id">User ID</label>
+        <input name="user-id" />
+
+        <br />
+
+        <label htmlFor="user-data-csv-file">User Data (.csv)</label>
+        <input type="file" name="user-data-csv-file" accept=".csv" />
+
+        <br />
+
+        <input type="submit" value="Submit" />
+      </form>
+    </PageContainer>
+  )
 }
