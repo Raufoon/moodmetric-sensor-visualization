49b99c986b6d6d0e8ba4f85f59aa54c609423e4d
Minhaz Raufoon <minhaz.raufoon@s2019.tu-chemnitz.de>
2021-12-26 19:42:53 +0100

    Add routes and nav bar

--- server/pages/index.js
+++ server/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -15,6 +16,16 @@ export default function Home() {
         <label>
           Moodmetric Sensor Visualization
         </label>
+
+        <nav>
+          <Link href="/">
+            <a>Graph</a>
+          </Link>
+
+          <Link href="/upload-data">
+            <a>Upload Sensor Data</a>
+          </Link>
+        </nav>
       </header>
 
       <main className={styles.main}>
