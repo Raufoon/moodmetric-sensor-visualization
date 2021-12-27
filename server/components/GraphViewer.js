e713362743cd9cb50627957f96ea63c08b69b739
Minhaz Raufoon <minhaz.raufoon@s2019.tu-chemnitz.de>
2021-12-27 05:17:40 +0100

    Add graph viewer event handlers

--- server/components/GraphViewer.js
+++ server/components/GraphViewer.js
@@ -1,22 +1,66 @@
+import { useState } from "react"
+import axios from "axios"
 import styles from '../styles/GraphViewer.module.css'
 
 export default function GraphViewer() {
+  const [userId, setUserId] = useState("")
+  const [startTimestamp, setStartTimestamp] = useState(0)
+  const [endTimestamp, setEndTimestamp] = useState(0)
+
+  function onChange(event) {
+    switch (event.target.name) {
+      case "userId":
+        setUserId(event.target.value)
+        break
+
+      case "startTimestamp":
+        setStartTimestamp(event.target.value)
+        break
+
+      case "endTimestamp":
+        setEndTimestamp(event.target.value)
+        break
+    }
+  }
+
+  async function onSubmit(event) {
+    event.preventDefault()
+
+    try {
+      const response = await axios.post("api/data-by-time-range", {
+        userId,
+        startTimestampMs: new Date(startTimestamp).getTime() / 1000,
+        endTimestampMs: new Date(endTimestamp).getTime() / 1000
+      })
+
+      if (response.success) {
+        console.response(data)
+      }
+      else {
+        throw new Error()
+      }
+    }
+    catch (err) {
+      window.alert("Failed to query")
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <div className={styles.control}>
+      <form className={styles.control} onSubmit={onSubmit}>
         <label>User ID</label>
         <label>Begin</label>
         <label>End</label>
         <label>&nbsp;</label>
 
-        <input name="userId" placeholder="at least 5 characters" />
-        <input name="startTimestamp" type="datetime-local" />
-        <input name="endTimestamp" type="datetime-local" />
+        <input name="userId" onChange={onChange} placeholder="at least 5 characters" />
+        <input name="startTimestamp" onChange={onChange} type="datetime-local" />
+        <input name="endTimestamp" onChange={onChange} type="datetime-local" />
 
         <div>
           <input type="submit" value="Load" />
         </div>
-      </div>
+      </form>
     </div>
   )
 }
